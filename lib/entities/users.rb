50cd8dba815803bdb9f24841572f3b763ddfc157
Christine Oen <oen.christine@gmail.com>
2014-08-16 14:27:05 -0500

    added database methods

--- lib/entities/users.rb
+++ lib/entities/users.rb
@@ -1,15 +1,15 @@
 module WorkoutBuddies
   class User
-    attr_accessor :user_id, :displayname, :address, :zip, :email, :phone, :gogole_info, :activities, :activity_ids
+    attr_accessor :user_id, :display_name, :address, :zip, :email, :phone, :refresh_token, :activities, :activity_ids
 
     def initialize(data = {})
       @user_id = data['user_id']
-      @displayname = data['username']
+      @display_name = data['display_name']
       @address = data['address']
       @zip = data['zip']
       @email = data['email']
       @phone = data['phone']
-      @google_info = data['google_info']
+      @refresh_token = data['refresh_token']
       @activity_ids = []
       @activities = []
     end
