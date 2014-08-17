22f0ba4a7010d0f894c79bec29c6449b3684ca51
Catherine O'Neill <cathzozo@gmail.com>
2014-08-16 20:42:55 -0500

    edit signup script

--- lib/scripts/signup.rb
+++ lib/scripts/signup.rb
@@ -3,7 +3,7 @@ require 'pony'
 module RPS
   class SignUp
     def self.run(params)
-      if params['email'].empty? || params['password'].empty? || params['password_conf'].empty? || params['screenname'].empty?
+      if params['email'].empty? || params['password'].empty? || params['password_conf'].empty? || params['display_name'].empty?
         return {:success? => false, :error => "EMPTY FIELDS"}
       elsif RPS::DBI.dbi.user_exists?(params['email'])
         return {:success? => false, :error => "USER ALREADY EXISTS"}
@@ -11,28 +11,29 @@ module RPS
         return {:success? => false, :error => "PASSWORDS DONT MATCH"}
       end
 
+##### Make a data hash to put in user new.
       email = params['email'].downcase.strip #need for Gravatar
-      user = RPS::User.new(params['screenname'], email)
+      user = RPS::User.new(data)
       user.update_password(params['password'])
       user.set_profile_pic
       RPS::DBI.dbi.save_user(user)
 
-      Pony.mail(
-        :to => user.email,   
-        :via => :smtp,
-          :via_options => {
-            :address              => 'smtp.gmail.com',
-            :port                 => '587',
-            :enable_starttls_auto => true,
-            :user_name            => 'rps.makersquare',
-            :password             => 'rockpaperscissors',
-            :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-            :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-            },
-          :from => 'rps.makersquare@gmail.com', 
-          :subject => 'Rock, Paper, Scissors', 
-          :body => "Thanks for registering with the Rock, Paper, Scissors game.  We know you'll love it! \n--the RPS team "
-          )
+      # Pony.mail(
+      #   :to => user.email,   
+      #   :via => :smtp,
+      #     :via_options => {
+      #       :address              => 'smtp.gmail.com',
+      #       :port                 => '587',
+      #       :enable_starttls_auto => true,
+      #       :user_name            => 'rps.makersquare',
+      #       :password             => 'rockpaperscissors',
+      #       :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      #       :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+      #       },
+      #     :from => 'rps.makersquare@gmail.com', 
+      #     :subject => 'Rock, Paper, Scissors', 
+      #     :body => "Thanks for registering with the Rock, Paper, Scissors game.  We know you'll love it! \n--the RPS team "
+      #     )
       
       {
         :success? => true,
