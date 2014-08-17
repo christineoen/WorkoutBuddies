a0f1577cb1c6f7e2a3287509d6a1a33f2baceec5
Catherine O'Neill <cathzozo@gmail.com>
2014-08-17 00:17:48 -0500

    small fixes in signin and signup

--- lib/scripts/signup.rb
+++ lib/scripts/signup.rb
@@ -5,7 +5,7 @@ module WorkoutBuddies
     def self.run(params)
       if params['email'].empty? || params['display_name'].empty? || params['password'].empty? || params['password_confirm'].empty?
         return {:success? => false, :error => "Please fill out all input fields."}
-      elsif WorkoutBuddies.dbi.email_exists?(params['email'])
+      elsif WorkoutBuddies::DBI.dbi.email_exists?(params['email'])
         return {:success? => false, :error => "Email address already registered. Please sign in."}
       elsif params['password'] != params['password_confirm']
         return {:success? => false, :error => "Passwords don't match. Please try again."}
