0e5c8d3aaf1bef10e9c702d3df636c1cd2ac9a4b
Christine Oen <oen.christine@gmail.com>
2014-08-17 11:42:08 -0500

    added logout functionality

--- lib/entities/events.rb
+++ lib/entities/events.rb
@@ -1,11 +1,12 @@
 module WorkoutBuddies
   class Event
-    attr_accessor :event_id, :event_name, :address, :zip, :user_id, :event_description
+    attr_accessor :event_id, :event_name, :address, :zip, :user_id, :event_description, :activity_id
 
     def initialize(data = {})
       @event_id = data['event_id']
       @event_name = data['event_name']
       @event_description = data['event_description']
+      @activity_id = data['activity_id']
       @address = data['address']
       @zip = data['zip']
       @user_id = data['user_id']
